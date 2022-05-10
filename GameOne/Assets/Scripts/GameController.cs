a66b65e0d1f3ebedf0a77236bbaec00e3ed69041
GabrielVRV <gabrielvrv1910@gmail.com>
2022-05-10 16:29:48 -0300

    Added an indication of how many stars there are in the level

--- GameOne/Assets/Scripts/GameController.cs
+++ GameOne/Assets/Scripts/GameController.cs
@@ -10,15 +10,20 @@ public class GameController : MonoBehaviour
     public Text scoreText;
     public Text scoreTextFinal;
 
+    public int amountStars;
+
     public static GameController instance;
     // Start is called before the first frame update
     void Start()
     {
+        TotalScore = 0;
         instance = this;
+        scoreText.text = TotalScore.ToString() + "/" + amountStars.ToString();
     }
 
     public void UpdateScoreText(){
-        scoreText.text = TotalScore.ToString();
+        scoreText.text = TotalScore.ToString() + "/" + amountStars.ToString();
+
         //scoreTextFinal.text = TotalScore.ToString();
     }
 
